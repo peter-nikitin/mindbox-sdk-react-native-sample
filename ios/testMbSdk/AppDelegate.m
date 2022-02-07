5c35c82f1bb94ad91430004428bde1de0a246224
Petr <nikitin.p.94@gmail.com>
2022-02-07 14:48:05 +0300

    Add Mindbox Init code

--- ios/testMbSdk/AppDelegate.m
+++ ios/testMbSdk/AppDelegate.m
@@ -12,6 +12,9 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
+@import Mindbox;
+@import MindboxSdk;
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -47,7 +50,74 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  // MINDBOX INTEGRATION
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+  [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    if (!error) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        [[Mindbox shared] notificationsRequestAuthorizationWithGranted:granted];
+      });
+    }
+    else {
+      NSLog(@"NotificationsRequestAuthorization failed with error: %@", error.localizedDescription);
+    }
+  }];
+  
+  
+  // MINDBOX INTEGRATION
+  if (@available(iOS 13.0, *)) {
+    [[Mindbox shared] registerBGTasks];
+  }
+  else {
+    [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+  }
+  
+  
+  // MINDBOX INTEGRATION
+  TrackVisitData *data = [[TrackVisitData alloc] init];
+  data.launchOptions = launchOptions;
+  [[Mindbox shared] trackWithData:data];
+
+  
+  
   return YES;
+}
+
+
+// MINDBOX INTEGRATION
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+  TrackVisitData *data = [[TrackVisitData alloc] init];
+  data.universalLink = userActivity;
+  [[Mindbox shared] trackWithData:data];
+  return YES;
+}
+
+// MINDBOX INTEGRATION
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+  completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionBadge);
+}
+
+// MINDBOX INTEGRATION
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [[Mindbox shared] apnsTokenUpdateWithDeviceToken:deviceToken];
+}
+
+// MINDBOX INTEGRATION
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+  [MindboxJsDelivery emitEvent:response];
+  [[Mindbox shared] pushClickedWithResponse:response];
+  TrackVisitData *data = [[TrackVisitData alloc] init];
+  data.push = response;
+  [[Mindbox shared] trackWithData:data];
+  completionHandler();
+}
+
+// MINDBOX INTEGRATION
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+  [[Mindbox shared] application:application performFetchWithCompletionHandler:completionHandler];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
